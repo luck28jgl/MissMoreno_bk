28245e5e15fa0b9d64cbb20da83ab14351daa969
JGL <luckibarra15@gmail.com>
2025-05-06 16:21:26 -0600

    Primer commit del prollecto

---
+++ core/models.py
@@ -0,0 +1,44 @@
+from django.contrib.postgres.operations import HStoreExtension, UnaccentExtension
+from django.contrib.postgres.fields import HStoreField
+from django.contrib.auth.models import User
+from django.db import models, migrations
+# Create your models here.
+class Migration(migrations.Migration):
+	operations = [
+		HStoreExtension(),
+		UnaccentExtension(),
+	]
+
+class usuario(models.Model):
+    class TiposUsuario(models.IntegerChoices):
+        MAESTRO = 1
+        ALUMNO = 2
+    usr = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    tipo_usuario = models.IntegerField(
+    choices=TiposUsuario.choices, default=TiposUsuario.ALUMNO)
+    tareastes = models.ForeignKey('tareas', on_delete=models.CASCADE, null=True)
+    bocabularioasing = models.ForeignKey('bocabulario', on_delete=models.CASCADE, null=True)
+    grupo = models.ForeignKey('Grupo', on_delete=models.SET_NULL, null=True, blank=True)
+
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    tareas = models.ManyToManyField('tareas', blank=True)
+
+class bocabulario(models.Model):
+    nombre = models.CharField(max_length=300, default='')
+    descripcion = models.CharField(max_length=300, default='')
+    texturl = models.CharField(max_length=200, default='')
+    texestañol = models.CharField(max_length=200, default='')
+    texingles = models.CharField(max_length=200, default='')
+    publico = models.BooleanField(default=False)
+
+class tareas(models.Model):
+    nombre = models.CharField(max_length=300, default='')
+    descripcion = models.CharField(max_length=300, default='')
+    es_general = models.BooleanField(default=False)
+
+class habilidadverbal(models.Model):
+    calificacion_max = models.CharField(max_length=300, default='')
+    caliicacion_minima = models.CharField(max_length=300, default='')
+    fecha_entrega = models.DateTimeField(null=True)
